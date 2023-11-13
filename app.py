0bb4531eb06329cb733efe86262427a5377ef700
Chris Taylor <ctaylor83@gmail.com>
2023-11-13 11:37:57 +0000

    Basic backend & frontends

--- app.py
+++ app.py
@@ -1,14 +1,58 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from models import Item, db
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+db.init_app(app)
 
 @app.route('/')
 def home():
-    return "Welcome to the Woodworking Inventory System!"
+    return render_template('index.html')
+
+@app.route('/add_item', methods=['POST'])
+def add_item():
+    name = request.form['name']
+    quantity = request.form['quantity']
+    item_type = request.form['type']  # New field
+    dimensions = request.form['dimensions']  # New field
+
+    new_item = Item(name=name, quantity=quantity, type=item_type, dimensions=dimensions)
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    return 'Item added successfully!'
+
+@app.route('/items', methods=['GET'])
+def view_items():
+    items = Item.query.all()
+    item_list = [{"id": item.id, "name": item.name, "quantity": item.quantity, "type": item.type, "dimensions": item.dimensions} for item in items]
+    return jsonify(item_list)
+
+@app.route('/update_item/<int:item_id>', methods=['POST'])
+def update_item(item_id):
+    item = Item.query.get_or_404(item_id)
+
+    item.name = request.form['name']
+    item.quantity = request.form['quantity']
+    item.type = request.form['type']  # Update the type
+    item.dimensions = request.form['dimensions']  # Update the dimensions
+
+    db.session.commit()
+
+    return 'Item updated successfully!'
+
+@app.route('/delete_item/<int:item_id>', methods=['POST'])
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return 'Item deleted successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,12 +1,15 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    # Additional fields like category, etc.
+    type = db.Column(db.String(80))  # For categorizing items (e.g., fixings, materials)
+    dimensions = db.Column(db.String(80)) 
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    # Define relationships and additional fields
+    # Define additional fields and relationships here if needed
